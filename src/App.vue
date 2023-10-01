845bf06183dc9a38faba92288c18a4de89f0bd96
Jimmy Lee <jimmylee850328@gmail.com>
2023-10-01 18:31:28 +0800

    feat: add view on github button

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,33 @@
 </script>
 
 <template>
-    <router-view />
+    <v-app>
+        <div>
+            <router-view />
+        </div>
+        <v-footer
+            app
+            class="bg-transparent d-flex justify-end mb-2"
+        >
+            <v-btn
+                class="text-none"
+                size="small"
+                @click="view_on_github"
+            > 
+                <v-icon class="pr-2">
+                    mdi-github
+                </v-icon>
+                View on Github
+            </v-btn>
+        </v-footer>
+    </v-app>
 </template>
 
-<style scoped>
+<script setup>
+    const view_on_github = () => {
+        open("https://github.com/jimmylee850328/frontend-portfolio/", "_blank");
+    };
+</script>
 
+<style scoped>
 </style>
